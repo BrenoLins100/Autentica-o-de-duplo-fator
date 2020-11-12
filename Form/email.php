8bd8758e0b3706cdaa70521f8bd98db8372ca17d
Breno Lins <36646169+BrenoLins100@users.noreply.github.com>
2020-11-12 17:13:53 -0300

    Update email.php

--- Form/email.php
+++ Form/email.php
@@ -1,28 +1,16 @@
+
+
 <?php
 
-$email = $_POST['email'];
-$senha = $_POST['senha'];
-$mensagem = "Olá";
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "test@hostinger-tutorials.com";
+    $to = "brenolins100@gmail.com";
+    $subject = "Checking PHP mail";
+    $message = "PHP mail works just fine";
+    $headers = "From:" . $from;
+    mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
 
-//enviar
-
-  // emails para quem será enviado o formulário
-  $emailenviar = "brenolins100@gmail.com";
-  $destino = $emailenviar;
-  $assunto = "Contato pelo Site";
-
-  // É necessário indicar que o formato do e-mail é html
-  $headers  = 'MIME-Version: 1.0' . "\r\n";
-      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-      $headers .= 'From: $nome <$email>';
-  //$headers .= "Bcc: $EmailPadrao\r\n";
-
-  $enviaremail = mail($destino, $assunto,$headers);
-  if($enviaremail){
-  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
-  echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
-  } else {
-  $mgm = "ERRO AO ENVIAR E-MAIL!";
-  echo "";
-  }
 ?>
+
